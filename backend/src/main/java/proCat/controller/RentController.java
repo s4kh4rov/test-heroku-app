008bbde4c995949a7af49d0438da95afde18d259
Alexander Sakharov <sakharov487@mail.ru>
2021-06-13 13:19:19 +0300

    test commit

--- backend/src/main/java/proCat/controller/RentController.java
+++ backend/src/main/java/proCat/controller/RentController.java
@@ -5,30 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import proCat.dto.RentStationDTO;
-import proCat.mapper.RentStationMapper;
-import proCat.service.RentStationService;
-
-import java.util.List;
 
 @RestController
 public class RentController {
-    private final RentStationMapper rentStationMapper;
-    private final RentStationService rentStationService;
 
     @Autowired
-    public RentController(RentStationMapper rentStationMapper, RentStationService rentStationService) {
-        this.rentStationMapper = rentStationMapper;
-        this.rentStationService = rentStationService;
-    }
+    public RentController() {
 
+    }
     @GetMapping("station/list")
-    public ResponseEntity<List<RentStationDTO>> getRentStationsList() {
-        List<RentStationDTO> stations = rentStationMapper.toRentStationDTOs(rentStationService.findAll());
-        if (stations.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(stations, HttpStatus.OK);
+    public ResponseEntity<String> getRentStationsList() {
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
     }
 
 }
